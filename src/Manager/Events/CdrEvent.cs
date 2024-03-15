b2c891f132a828098673e66ae890d35c1989f4ba
Jan Bartula <jan.bartula@allmedica.pl>
2024-03-15 12:31:45 +0100

    CdrEvent UniqueId

--- src/Manager/Events/CdrEvent.cs
+++ src/Manager/Events/CdrEvent.cs
@@ -40,6 +40,8 @@ namespace Sufficit.Asterisk.Manager.Events
 
         public string AmaFlags { get; set; }
 
+        public string UniqueID { get; set; }
+
         public string UserField { get; set; }
     }
 }
